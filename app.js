8257f3f48d3ee3e54603885d595283ed923a3ba1
Calix Huang <calix.huang1@gmail.com>
2020-05-18 20:50:51 -0700

    removed unnecessary file system code

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
-const express = require("express")
-const fs = require("fs")
-const readline = require('readline');
-var Transform = require('stream').Transform;
-var util = require('util');
+const express = require("express");
+const mongodb = require("mongodb");
 
-const questionsFilePath = "./questions.txt";
 const app = express()
-
-async function removeFirstLine() {
-  fs.readFile(questionsFilePath, function(err, data) { // read file to memory
-      if (!err) {
-          data = data.toString(); // stringify buffer
-          var position = data.toString().indexOf('\n'); // find position of new line element
-          if (position != -1) { // if new line element found
-              data = data.substr(position + 1); // subtract string based on first line length
-
-              fs.writeFile(questionsFilePath, data, function(err) { // write file
-                if (err) { // if error, report
-                  console.log (err);
-                }
-              });
-          } else {
-              console.log('no lines found');
-          }
-      } else {
-          console.log(err);
-      }
-  });
-}
-
-async function getFirstLine(pathToFile) {
-  const readable = fs.createReadStream(pathToFile);
-  const reader = readline.createInterface({ input: readable });
-  const line = await new Promise((resolve) => {
-    reader.on('line', (line) => {
-      reader.close();
-      resolve(line);
-    });
-  });
-  readable.close();
-  return line;
-}
+const MongoClient = mongodb.MongoClient;
 
 app.get("/", (req, res) => {
-  res.send("we be hackin' CVT")
+  res.send("bots boi")
 });
 
 app.post("/dailyquestion", (req, res) => {
-  getFirstLine(questionsFilePath).then((line) => {
-    res.send({
-      response_type: "in_channel",
-      text: line
-    });
-
-    removeFirstLine();
+  res.send({
+    response_type: "in_channel",
+    text: "Currently under development..."
   });
 });
 
